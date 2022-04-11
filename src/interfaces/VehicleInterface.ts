d1ff466749eb17541e6e6d886daa910e0361b10d
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-11 17:58:46 -0300

    :label: fix: ajusta tipagem e validações

--- src/interfaces/VehicleInterface.ts
+++ src/interfaces/VehicleInterface.ts
@@ -1,7 +1,11 @@
-export interface Vehicle {
-  model: string,
-  year: number,
-  color: string,
-  status?: boolean,
-  buyValue: number,
-}
+import { z as zod } from 'zod';
+
+export const VehicleScheme = zod.object({
+  model: zod.string().min(3),
+  year: zod.number().min(1900).max(2022),
+  color: zod.string().min(3),
+  status: zod.boolean().optional(),
+  buyValue: zod.number().int(),
+});
+
+export type Vehicle = zod.infer<typeof VehicleScheme>;
