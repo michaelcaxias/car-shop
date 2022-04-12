3e1934c59ec8b3bb4caa99126ce3e46b76c47284
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-12 15:45:20 -0300

    fix: ajusta importação de carScheme

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,7 +1,7 @@
 import { z as zod } from 'zod';
 import { VehicleScheme } from './VehicleInterface';
 
-const carScheme = VehicleScheme.extend({
+export const carScheme = VehicleScheme.extend({
   doorsQty: zod.number().min(2).max(4),
   seatsQty: zod.number().min(2).max(7),
 });
