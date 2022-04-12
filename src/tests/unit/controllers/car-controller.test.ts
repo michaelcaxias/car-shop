c7ddfb6eaeb9cb4685718c09b802731acbebe313
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-12 19:26:41 -0300

    test: adiciona novos casos de testes para cobrir camada de controllers

--- src/tests/unit/controllers/car-controller.test.ts
+++ src/tests/unit/controllers/car-controller.test.ts
@@ -6,7 +6,7 @@ import CarService from "../../../services/CarServices";
 import { Car } from "../../../interfaces/CarInterface";
 import { ControllerErrors, RequestWithBody } from "../../../controllers";
 import { Response } from "express";
-import { createSucessfullResponse, sucessfulCarPayload } from "../mocks/car-mocks";
+import { createSucessfullResponse, errorMock, sucessfulCarPayload } from "../mocks/car-mocks";
 
 chai.use(chaiHttp);
 
@@ -40,8 +40,13 @@ describe("Car - Camada de Controllers", () => {
       await carControllers.create(request, response);
       expect((response.json as sinon.SinonStub).calledWith(createSucessfullResponse)).to.be.true;
     });
+
+    it("Verifica se a rota é /cars", () => {
+      const route = carControllers.route;
+      expect(route).to.be.equal('/cars')
+    })
   });
-  describe('Testes em caso de falha', () => {
+  describe('Testes em caso de falha ao chegar "undefined" do create', () => {
     const carControllers = new CarController();
     const request = {} as RequestWithBody<Car>;
     const response = {} as Response;
@@ -49,6 +54,30 @@ describe("Car - Camada de Controllers", () => {
     response.json = sinon.stub();
     before(() => {
       sinon.stub(carControllers.service, "create").resolves(undefined);
+    });
+
+    after(() => {
+      (carControllers.service.create as sinon.SinonStub).restore()
+      sinon.restore();
+    });
+
+    it("Verifica se o método create está retornando o status 400", async () => {
+      await carControllers.create(request, response);
+      expect((response.status as sinon.SinonStub).calledWith(400)).to.be.true;
+    });
+    it("Verifica se o método create está retornando 'Bad Request'", async () => {
+      await carControllers.create(request, response);
+      expect((response.json as sinon.SinonStub).calledWith({ error: ControllerErrors.badRequest })).to.be.true;
+    });
+  })
+  describe('Testes em caso de falha ao chegar um erro Zod do create', () => {
+    const carControllers = new CarController();
+    const request = {} as RequestWithBody<Car>;
+    const response = {} as Response;
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub();
+    before(() => {
+      sinon.stub(carControllers.service, "create").resolves(errorMock as never);
     });
 
     after(() => {
