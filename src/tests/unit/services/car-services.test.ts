bab82283dfa45b76d402f2f7425f64ca1f42d77b
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-12 18:55:28 -0300

    feat: corrige importação do mongo de connection

--- src/tests/unit/services/car-services.test.ts
+++ src/tests/unit/services/car-services.test.ts
@@ -2,11 +2,12 @@ import { expect } from 'chai';
 import mongoose from 'mongoose';
 import * as sinon from "sinon";
 import CarService from '../../../services/CarServices';
-import { createSucessfullResponse } from '../mocks/car-mocks';
+import { createSucessfullResponse, sucessfulCarPayload } from '../mocks/car-mocks';
+import { carScheme } from '../../../interfaces/CarInterface';
 
 describe("Car - Camada de Services", () => {
   const carService = new CarService();
-  describe("Testes em caso de sucesso", () => {
+  describe("Testes em método create em caso de sucesso", () => {
     before(() => {
       sinon.stub(carService, "create").resolves(createSucessfullResponse);
     });
@@ -16,16 +17,17 @@ describe("Car - Camada de Services", () => {
       sinon.restore();
     });
 
-    it("...", async () => {
+    it("Verifica se o retorno do método create está com o payload correto", async () => {
+      const carData = await carService.create(sucessfulCarPayload);
+      expect(carData).to.be.deep.equal(createSucessfullResponse);
     });
   });
-  describe('Testes em caso de falha', () => {
+  describe('Testes em método create em casos de falha', () => {
     before(() => {
-      sinon.stub(carService, "create").resolves(createSucessfullResponse);
+      sinon.stub(carScheme, "safeParse").resolves(createSucessfullResponse);
     });
 
     after(() => {
-      (carService.create as sinon.SinonStub).restore()
       sinon.restore();
     });
 
