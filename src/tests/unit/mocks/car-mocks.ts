992e0e6d31b444d3f156c853e24a40c5a10c5448
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-12 17:00:44 -0300

    feat: cria arquivo para concentrar mocks de cars

---
+++ src/tests/unit/mocks/car-mocks.ts
@@ -0,0 +1,11 @@
+export const getSucessfullResponse = {
+  model: "bmw",
+  year: 1994,
+  color: "blue",
+  status: "true",
+  buyValue: 52224,
+  doorsQty: 2,
+  seatsQty: 2,
+  _id: "6255da496e0a5a5f87d43e72",
+  __v: 0,
+};
