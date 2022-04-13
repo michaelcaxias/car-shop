fa89f99f032ef71b0b10fc13e66ff1d56c34aee3
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-13 17:15:27 -0300

    test: cria função para verificar erros internos

--- src/tests/unit/controllers/car-controller.test.ts
+++ src/tests/unit/controllers/car-controller.test.ts
@@ -6,6 +6,7 @@ import { Car } from "../../../interfaces/CarInterface";
 import { ControllerErrors, RequestWithBody } from "../../../controllers";
 import { Response } from "express";
 import { createSucessfullResponse, errorMock, readCars, sucessfulCarPayload } from "../mocks/car-mocks";
+import { verifyInternalError, verifyResponseSucessfully } from '../utils/index';
 
 chai.use(chaiHttp);
 
@@ -32,7 +33,6 @@ describe("Car - Camada de Controllers", () => {
     it("Verifica se o método CREATE está retornando o status 201", async () => {
       await carControllers.create(request, response);
       expect((response.status as sinon.SinonStub).calledWith(201)).to.be.true;
-      expect((response.json as sinon.SinonStub).calledWith(createSucessfullResponse)).to.be.true;
     });
 
 
@@ -84,43 +84,11 @@ describe("Car - Camada de Controllers", () => {
       expect((response.json as sinon.SinonStub).calledWith({ error: ControllerErrors.badRequest })).to.be.true;
     });
   })
-  describe('Em caso de FALHA no caso do service lançar um erro no método CREATE', () => {
-
-    before(() => sinon.stub(carControllers.service, "create").throws());
-
-    after(() => {
-      (carControllers.service.create as sinon.SinonStub).restore()
-      sinon.restore();
-    });
-
-    it("Verifica se o método CREATE está retornando o status 500", async () => {
-      await carControllers.create(request, response);
-      expect((response.status as sinon.SinonStub).calledWith(500)).to.be.true;
-    });
-    it("Verifica se o método CREATE está retornando um erro interno", async () => {
-      await carControllers.create(request, response);
-      expect((response.json as sinon.SinonStub).calledWith({ error: ControllerErrors.internal })).to.be.true;
-    });
+  describe('Em caso de FALHA de erro interno no método CREATE', () => {
+    verifyInternalError({ method: 'create'});
   })
   describe("Em caso de SUCESSO do método READ", () => {
-
-    before(() => sinon.stub(carControllers.service, "read").resolves(readCars));
-
-    after(() => {
-      (carControllers.service.read as sinon.SinonStub).restore()
-      sinon.restore();
-    });
-
-    it("Verifica se o método READ está retornando o status 200", async () => {
-      await carControllers.read(request, response);
-      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.true;
-    });
-
-
-    it("Verifica se o método READ está retornando o body esperado", async () => {
-      await carControllers.read(request, response);
-      expect((response.json as sinon.SinonStub).calledWith(readCars)).to.be.true;
-    });
+    verifyResponseSucessfully({ method: 'read', mockResponse: readCars, status: 200 })
   });
   describe("Em caso de FALHA no caso do service retornar null no método READ", () => {
 
@@ -142,24 +110,10 @@ describe("Car - Camada de Controllers", () => {
       expect((response.json as sinon.SinonStub).calledWith({ error: ControllerErrors.notFound })).to.be.true;
     });
   });
-  describe("Em caso de FALHA no caso do service lançar um erro no método READ", () => {
-
-    before(() => sinon.stub(carControllers.service, "read").throws('Um erro ocorreu'))
-
-    after(() => {
-      (carControllers.service.read as sinon.SinonStub).restore()
-      sinon.restore();
-    });
-
-    it("Verifica se o método READ está retornando o status 500", async () => {
-      await carControllers.read(request, response);
-      expect((response.status as sinon.SinonStub).calledWith(500)).to.be.true;
-    });
-
-
-    it("Verifica se o método READ está retornando um erro interno", async () => {
-      await carControllers.read(request, response);
-      expect((response.json as sinon.SinonStub).calledWith({ error: ControllerErrors.internal })).to.be.true;
-    });
+  describe("Em caso de FALHA nde erro interno no método READ", () => {
+    verifyInternalError({ method:  'read'});
+  });
+  describe("Em caso de FALHA de erro interno no método READONE", () => {
+    verifyInternalError({ method: 'readOne'});
   });
 });
--- src/tests/unit/utils/index.ts
+++ src/tests/unit/utils/index.ts
@@ -4,17 +4,19 @@ import chaiHttp = require("chai-http");
 import CarController from "../../../controllers/CarController";
 import { Car } from "../../../interfaces/CarInterface";
 import { ControllerErrors, RequestWithBody } from "../../../controllers";
-import { Response } from "express";
+import { Response, Request } from "express";
 
 chai.use(chaiHttp);
 const { expect } = chai;
 
 type Methods = 'read' | 'readOne' | 'create';
+type sucessfullyObject = { method: Methods, mockResponse: any, status: number }
 
-export const verifyInternalError = (method: Methods) => {
+export const verifyInternalError = ({ method }: { method: Methods }) => {
   const carControllers = new CarController();
-  const request = {} as RequestWithBody<Car>;
+  const request = {} as Request;
   const response = {} as Response;
+  if (method === 'readOne') { request.params = { id: '123456' } }
   response.status = sinon.stub().returns(response);
   response.json = sinon.stub();
 
@@ -34,5 +36,31 @@ export const verifyInternalError = (method: Methods) => {
   it(`Verifica se o método ${method.toUpperCase()} está retornando um erro interno`, async () => {
     await carControllers[method](request, response);
     expect((response.json as sinon.SinonStub).calledWith({ error: ControllerErrors.internal })).to.be.true;
+  });
+}
+
+export const verifyResponseSucessfully = ({ method, mockResponse, status }: sucessfullyObject) => {
+  const carControllers = new CarController();
+  const request = {} as RequestWithBody<Car>;
+  const response = {} as Response;
+  response.status = sinon.stub().returns(response);
+  response.json = sinon.stub();
+
+  before(() => sinon.stub(carControllers.service, method).resolves(mockResponse));
+
+  after(() => {
+    (carControllers.service[method] as sinon.SinonStub).restore()
+    sinon.restore();
+  });
+
+  it(`Verifica se o método ${method} está retornando o status ${status}`, async () => {
+    await carControllers[method](request, response);
+    expect((response.status as sinon.SinonStub).calledWith(status)).to.be.true;
+  });
+
+
+  it(`Verifica se o método ${method} está retornando o body esperado`, async () => {
+    await carControllers[method](request, response);
+    expect((response.json as sinon.SinonStub).calledWith(mockResponse)).to.be.true;
   });
 }
