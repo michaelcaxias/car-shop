626838fe0528381e7743b9f12a1e6cb642e07b15
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-04-18 16:35:42 -0300

    :test_tube: test: cria testes para método readOne em models

--- src/tests/unit/models/car-models.test.ts
+++ src/tests/unit/models/car-models.test.ts
@@ -39,4 +39,20 @@ describe('Car - Camada de Models', () => {
       expect(response).to.deep.equal(readCars)
     })
   })
+
+  
+  describe('Em caso de sucesso do método READONE', () => {
+    before(() => {
+      sinon.stub(carModel, "readOne").resolves(readCars[0])
+    })
+
+    after(() => {
+      sinon.restore();
+    })
+
+    it("Verifica se o método READONE retorna o que era esperado", async () => {
+      const response = await carModel.readOne('6255f49b9c3c804d9a067e69');
+      expect(response).to.deep.equal(readCars[0])
+    })
+  })
 })
